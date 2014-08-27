7a68a801d6112eca1ccfd0b3ae2126ef48c4a999
Federico <ftejeiro@gmail.com>
2014-08-27 17:23:34 +0200

    Intentando que lea el fichero de internet

---
+++ DecisionTree_MIO.R
@@ -0,0 +1,72 @@
+# Trevor Stephens - 9 Jan 2014
+# Titanic: Getting Started With R - Part 2: The gender-class model
+# Full guide available at http://trevorstephens.com/
+
+
+# Set working directory 
+setwd("~/Personal/GITHUB/Titanic/Titanic")
+
+#import datafiles
+
+Mi_GIT.path <- "https://github.com/FedericoTe/Titanic/"
+Nombre.train.data.file <- "train.csv"
+Nombre.test.data.file <- "test.csv"
+missing.types <- c("NA", "")
+train.column.types <- c('integer',   # PassengerId
+                        'factor',    # Survived 
+                        'factor',    # Pclass
+                        'character', # Name
+                        'factor',    # Sex
+                        'numeric',   # Age
+                        'integer',   # SibSp
+                        'integer',   # Parch
+                        'character', # Ticket
+                        'numeric',   # Fare
+                        'character', # Cabin
+                        'factor'     # Embarked
+)
+test.column.types <- train.column.types[-2]     # # Como no existe la columna Survived in test.csv
+
+# This leaves me with much cleaner code for reading the csv files.
+
+train.raw <- read.csv(Titanic.path, train.data.file, train.column.types, missing.types)
+df.train <- train.raw
+
+test.raw <- readData(Titanic.path, test.data.file, 
+                     test.column.types, missing.types)
+df.test <- test.raw   
+
+# DATA MUNGING
+#FITTING A MODEL
+
+# Importamos un paquete previamente cargado, en este caso viene con r-base el paquete Recursive Partitioning and Regression Trees’ 
+
+library(rpart)
+
+# Ahora corremos el arbol que busca Survived relacionado con el resto de variables, tomando los data de df.train y como df.train$Survived es discreta (factor) usamos como method class, si fuera continua ponemos method="anova"
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=df.train, method="class")
+
+#Para entender y ver el arbol de decision creado vamos primero a cargar otros tres paquetes y luego los importamos:
+
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+#Ahora vemos el arbol con:
+
+plot(fit)
+text(fit)
+
+#Y gráficamente más bonito con
+
+fancyRpartPlot(fit)
+
+# Para crear ahora el fichero a enviar usamos una funcion predict del paquete rpart
+
+Prediction <- predict(fit, test, type = "class")
+submit <- data.frame(PassengerId = df.test$PassengerId, Survived = Prediction)
+write.csv(submit, file = "myfirstdtree.csv", row.names = FALSE)
